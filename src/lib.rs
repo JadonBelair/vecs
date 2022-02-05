27c6a9dace0eb97deff67496667988d5e5b27a62
JadonBelair <46850171+JadonBelair@users.noreply.github.com>
2022-02-05 18:49:48 -0500

    added more documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,38 @@
 //! Useful vector math implementations
+//! 
+//! # Examples
+//! 
+//! ```
+//! use vecs::Vec2;
+//! 
+//! fn main() {
+//!     // creates 2 Vec2 objects
+//!     let v1 = Vec2::new(12, 6);
+//!     let v2 = Vec2::new(17, 9);
+//! 
+//!     // adds the vectors together
+//!     let v3 = v1 + v2;
+//! 
+//!     // prints (29, 15) to the console
+//!     println!("{}", v3);
+//! }
+//! ```
+//! 
+//! ```
+//! use vecs::Vec3;
+//! 
+//! fn main() {
+//!     // creates 2 Vec3 objects
+//!     let v1 = Vec3::new(2, 6, 7);
+//!     let v2 = Vec3::new(5, 3, 8);
+//! 
+//!     // gets the 2 vectors cross product
+//!     let v3 = v1.cross(v2);
+//! 
+//!     // prints (27, 19, -24) to the console
+//!     println!("{}", v3);
+//! }
+//! ```
 
 use num::Num;
 use std::{fmt, ops::{Add, Sub, Neg}};
@@ -14,11 +48,38 @@ pub struct Vec2<T: Num> {
 
 impl<T: Num + Copy> Vec2<T> {
     /// returns a new Vec2 with the specified coordinates
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// use vecs::Vec2;
+    /// 
+    /// // creates a new Vec2 called v1
+    /// let v1 = Vec2::new(1, 2);
+    /// 
+    /// // creates a new Vec2 call v2
+    /// let v2 = Vec2::new(10, 20);
+    /// ```
     pub fn new(x: T, y: T) -> Vec2<T> {
         Vec2 { x, y }
     }
 
-    /// returns the dot product of 2 2d vectors
+    /// returns the dot product of 2 2D vectors
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// use vecs::Vec2;
+    /// 
+    /// // creates 2 new Vec2 objects
+    /// let v1 = Vec2::new(1, 2);
+    /// let v2 = Vec2::new(1, 2);
+    /// 
+    /// // stores their dot product
+    /// let d = v1.dot(v2);
+    /// 
+    /// assert_eq!(5, d);
+    /// ```
     pub fn dot(&self, other: Vec2<T>) -> T {
         self.x * other.x + self.y * other.y
     }
@@ -56,16 +117,58 @@ pub struct Vec3<T: Num> {
 
 impl<T: Num + Copy + Neg<Output = T>> Vec3<T> {
     /// returns a new Vec3 with the specified coordinates
+    ///
+    /// # Examples
+    /// 
+    /// ```
+    /// use vecs::Vec3;
+    /// 
+    /// // creates a new Vec3 called v1
+    /// let v1 = Vec3::new(1, 2, 3);
+    /// 
+    /// // creates a new Vec3 call v2
+    /// let v2 = Vec3::new(10, 20, 30);
+    /// ```
     pub fn new(x: T, y: T, z: T) -> Vec3<T> {
         Vec3 { x, y, z }
     }
 
     /// returns the dot product of 2 3D vectors
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// use vecs::Vec3;
+    /// 
+    /// // creates 2 new Vec3 objects
+    /// let v1 = Vec3::new(1, 2, 3);
+    /// let v2 = Vec3::new(1, 2, 3);
+    /// 
+    /// // stores their dot product
+    /// let d = v1.dot(v2);
+    /// 
+    /// assert_eq!(14, d);
+    /// ```
     pub fn dot(&self, other: Vec3<T>) -> T {
         self.x * other.x + self.y * other.y + self.z * other.z
     }
 
     /// returns the cross product of 2 3D vectors
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// use vecs::Vec3;
+    /// 
+    /// // creates 2 new Vec3 objects
+    /// let v1 = Vec3::new(3, 2, 1);
+    /// let v2 = Vec3::new(1, 2, 3);
+    /// 
+    /// // stores their cross product
+    /// let v3 = v1.cross(v2);
+    /// 
+    /// assert_eq!(Vec3::new(4, -8, 4), v3);
+    /// ```
     pub fn cross(&self, other: Vec3<T>) -> Vec3<T> {
         let x = (self.y * other.z) - (self.z * other.y);
         let y = (self.x * other.z) - (self.z * other.x);
